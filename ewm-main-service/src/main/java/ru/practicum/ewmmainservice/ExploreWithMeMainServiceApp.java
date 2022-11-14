7acb205c027849244abf88518d6e468bf2b044fd
Roman Zbruev <roman.zbruev@yandex.ru>
2022-11-15 00:52:13 +0300

    test commit

---
+++ ewm-main-service/src/main/java/ru/practicum/ewmmainservice/ExploreWithMeMainServiceApp.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewmmainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeMainServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMeMainServiceApp.class, args);
+    }
+}
